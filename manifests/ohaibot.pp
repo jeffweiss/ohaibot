f42d17ca101b7d1d699d55792de554cb37b23581
jeffweiss <jeff.weiss@puppetlabs.com>
2015-01-23 12:03:42 -0800

    get rid of vestigal vcsrepo stuff. vagrant sharing, duh

--- manifests/ohaibot.pp
+++ manifests/ohaibot.pp
@@ -16,20 +16,3 @@ package { 'elixir':
   ensure => latest,
   require => Apt::Source['erlang-solutions'],
 }
-
-package {'git':
-  ensure => latest,
-}
-
-#$ohaibot_dir = '/var/src/ohaibot'
-
-#file {"$ohaibot_dir":
-#ensure => directory,
-#}
-
-#vcsrepo {"$ohaibot_dir":
-#ensure   => present,
-#provider => git,
-#source   => 'https://github.com/jeffweiss/ohaibot.git',
-#require  => [Package['git'],File["$ohaibot_dir"]],
-#}
