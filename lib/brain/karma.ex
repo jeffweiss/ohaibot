bff5d5ed2e75eb90eca74d2ae4f10c63e9166ec0
Jeff Weiss <jeff.weiss@puppetlabs.com>
2015-02-19 23:38:28 +0000

    (#5) Use filesystem backing for Brain.Karma
    Prior to this commit, the karma brain would forget everything if the
    process was stopped/restarted. This commit creates a backup file,
    that is human-readable, on each karma change (this is terrible, but
    will work for now). The process also attempts to read the karma
    backup file on start up.
    
    Close #5.

--- lib/brain/karma.ex
+++ lib/brain/karma.ex
@@ -1,10 +1,20 @@
 defmodule Brain.Karma do
+  require Logger
   use GenServer
 
   def start_link(initial_state \\ []) do
     GenServer.start_link __MODULE__, [initial_state], name: __MODULE__
   end
 
+  def init([[]]) do
+    Logger.debug "No initial karma state provided, attempting to read from disk"
+    case :file.consult(data_backup_file) do
+      {:ok, state} -> {:ok, state}
+      _ ->
+        Logger.warn "No initial karma and can't read backup, starting with empty Karma Brain"
+        {:ok, []}
+    end
+  end
   def init([state]) do
     {:ok, state}
   end
@@ -21,6 +31,15 @@ defmodule Brain.Karma do
     {^subject, current_karma} = List.keyfind(state, subject, 0, {subject, 0})
     new_karma = current_karma + amount
     new_state = List.keystore(state, subject, 0, {subject, new_karma})
+    content = new_state
+    |> IO.inspect
+    |> Enum.map(&[:io_lib.print(&1) | ".\n"])
+    |> IO.iodata_to_binary
+    File.write(data_backup_file, content)
     {:reply, new_karma, new_state}
+  end
+
+  defp data_backup_file do
+    "data/karma/backup.dat"
   end
 end
