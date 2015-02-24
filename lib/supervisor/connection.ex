65f8aa207f412e0a4ea0a12c13a184d749ce711d
jeffweiss <jeff.weiss@puppetlabs.com>
2015-02-23 21:31:45 -0800

    (#7) Pull login info from config file
    Prior to this commit, the login information was hard-coded in the code.
    This commit reads the server, botname, channels to join, etc form config
    files upon startup.
    
    Close #7.

--- lib/supervisor/connection.ex
+++ lib/supervisor/connection.ex
@@ -10,10 +10,30 @@ defmodule Supervisor.Connection do
 
     children = [
       worker(ExIrc.Client, [[debug: true], [name: client]]),
-      worker(ConnectionHandler, [client]),
-      worker(LoginHandler, [client, ["#ohaibot-testing"]]),
+      worker(ConnectionHandler, [client, read_connection_config]),
+      worker(LoginHandler, [client, read_channel_list]),
       worker(Supervisor.Bot, [client])
       ]
     supervise(children, strategy: :one_for_all)
   end
+
+  defp read_connection_config(filename \\ "ohaibot.conf") do
+    if File.exists?(filename) do
+      {:ok, config} = :file.consult(filename)
+      as_map = for conf_item <- config, into: Map.new, do: conf_item
+      Map.merge(%ConnectionHandler.State{}, as_map)
+    else
+      %ConnectionHandler.State{}
+    end
+  end
+
+  defp read_channel_list(filename \\ "channels.conf") do
+    if File.exists?(filename) do
+      {:ok, channels} = :file.consult(filename)
+      channels
+    else
+      []
+    end
+  end
+
 end
